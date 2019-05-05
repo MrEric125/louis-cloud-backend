cf86c2910de36aa048e7062963875d99eade365f
80003996 <80003996@sfmail.sf-express.com>
2019-05-05 19:31:01 +0800

    添加dubbo和zuul

--- cloud-config/db-config-service/src/main/java/com/BdConfigApp.java
+++ cloud-config/db-config-service/src/main/java/com/BdConfigApp.java
@@ -2,7 +2,10 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Eric
@@ -13,7 +16,15 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class BdConfigApp {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(BdConfigApp.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
