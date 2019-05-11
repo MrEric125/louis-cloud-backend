5983cb128f32332514dbed9c45b0caae865ac620
MrEric125 <627460581@qq.com>
2019-05-11 20:15:36 +0800

    新增eleticsearch功能

---
+++ cloud-search-service/src/main/java/com/cloud/cloudsearchservice/CloudSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSearchServiceApplication.class, args);
+    }
+
+}
