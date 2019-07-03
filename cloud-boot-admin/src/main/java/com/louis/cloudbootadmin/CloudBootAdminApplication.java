415a8b084709720a9d6dfe319a84b4db3aef587b
80003996 <627460581@qq.com>
2019-07-03 19:07:54 +0800

    spring boot admin 依赖完善

--- cloud-boot-admin/src/main/java/com/louis/cloudbootadmin/CloudBootAdminApplication.java
+++ cloud-boot-admin/src/main/java/com/louis/cloudbootadmin/CloudBootAdminApplication.java
@@ -1,12 +1,14 @@
 package com.louis.cloudbootadmin;
 
-import com.louis.common.web.web.anontation.SpringCloudClient;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableAdminServer
-@SpringCloudClient
+@SpringCloudApplication
+@EnableHystrix
+
 public class CloudBootAdminApplication {
 
     public static void main(String[] args) {
