4650dc8a86084020def1d749cdef7cb5cb2a634a
80003996 <627460581@qq.com>
2019-07-17 15:09:03 +0800

    完善了模块依赖关系

---
+++ cloud-search-service/search-service-core/src/main/java/com/louis/es/service/ProductESService.java
@@ -0,0 +1,16 @@
+package com.louis.es.service;
+
+import com.louis.es.base.service.BaseEsCRUDService;
+import com.louis.es.entity.ProductDocument;
+
+
+/**
+ * @author louis
+ * <p>
+ * Date: 2019/7/17
+ * Description:
+ */
+public interface ProductESService extends BaseEsCRUDService<ProductDocument, Long> {
+
+
+}
